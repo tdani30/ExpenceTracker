db4a6f419811d030613e4a2e9961e092b7e61498
tdani30 <syedtalha30@gmail.com>
2020-06-22 15:44:53 +0400

    Move Everything to Git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Balance from './Balance';
+import Histroy from './History';
+import Transaction from './Transaction';
+import './CustomCss.css';
+import ApplicationContext from './AppContext';
 
-function App() {
+export default class App extends Component
+{
+  //{firstName:"John", lastName:"Doe", age:46}
+  constructor(props){
+    super(props);
+    this.state={
+      Histroy:[],
+      TotalSum:0,
+      PlusAmount:0,
+      MinusAmount:""
+  };
+  }
+  handleContextForUpdateState=(data,e)=>
+  {
+    debugger;
+    this.setState(
+            {
+                Histroy:[...this.state.Histroy,data]
+            });
+
+            let Totalsum=0;
+            let Plus=0;
+            let Minus=0;
+            const sjum =  this.state.Histroy.map((info,index)=>  
+            {
+              if(Math.sign(info.Amount)==1)
+              {
+              Plus+=parseInt(info.Amount,10);
+              }
+              else{
+                Minus+=parseInt(info.Amount,10);
+              }
+              Totalsum+=parseInt(info.Amount,10);
+            });
+            this.setState(
+              {
+                TotalSum:(Totalsum+(parseInt(data.Amount,10))),
+                MinusAmount:Math.sign(data.Amount)!=1 ? (Minus+(parseInt(data.Amount,10))).toString().replace('-',''):this.state.MinusAmount.toString().replace('-',''),
+                PlusAmount:Math.sign(data.Amount)==1 ?(Plus+(parseInt(data.Amount,10))):this.state.PlusAmount
+              });
+            e.preventDefault();
+  }
+
+  componentDidUpdate=()=>
+  {
+    debugger;
+   console.log("After Update");
+   console.log(this.state);
+  }
+   render(){
+    const contextData={
+      data:this.state,
+      handleForUpdateState:this.handleContextForUpdateState
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className="container">
+  <h1>Expence Tracker</h1>
+  <ApplicationContext.Provider value={contextData}>
+  <div>
+    <Balance></Balance>
+    <Histroy></Histroy>
+    <Transaction></Transaction>
+  </div>
+
+  </ApplicationContext.Provider>
+  </div>
+  </>
   );
+   }
 }
 
-export default App;
+// export default App;
---
+++ src/History.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import ApplicationContext from './AppContext';
+
+export default class History extends Component 
+{
+    constructor() {
+        super();
+        // this.state={
+        //     Histroy:[]
+        // };
+      }
+    //   handleContextForUpdateState=(data,ev)=>
+    //   {
+    //     debugger;
+    //   }
+    render() {
+        // const contextData={
+        //     data:this.state,
+        //     handleForUpdateState:handleContextForUpdateState
+        // };
+        return (
+            <ApplicationContext.Consumer>
+        {context => (
+            
+            <React.Fragment>
+                <h3>History</h3>
+                {console.log(context)}
+                <ul className="list">
+                    {context.data.Histroy.map((data,index)=>
+                    {
+                        return <li key={index} className={Math.sign(data.Amount)==1? "plus":"minus"}>{data.TransactionName}<span>{data.Amount}</span><button className="delete-btn">x</button></li>
+                    })}
+                </ul>
+            </React.Fragment>
+        )}
+    </ApplicationContext.Consumer>
+        )
+    }
+}
